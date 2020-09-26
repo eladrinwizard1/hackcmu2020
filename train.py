56fa0c3906e81b09665f2bace9dc09f0d50d1de2
Abhishek Vijayakumar <abhishev@andrew.cmu.edu>
2020-09-26 00:37:10 -0400

    minor tuning, results still not great

--- train.py
+++ train.py
@@ -9,12 +9,11 @@ from dataset import DATASETS
 
 def make_data():
     data = []
-    data += DATASETS["reviews"].sample(0)
-    data += DATASETS["shakespeare"].sample(0)
-    data += DATASETS["stackoverflow"].sample(0)
-    data += DATASETS["toptweets"].sample(0)
+    data += DATASETS["reviews"].sample(100)
+    data += DATASETS["shakespeare"].sample(100)
+    data += DATASETS["stackoverflow"].sample(1000)
+    data += DATASETS["toptweets"].sample(1000)
     data += DATASETS["bible"].sample(1000)
-    data += DATASETS["worm"].sample(10000)
     shuffle(data)
     return data
 
@@ -23,13 +22,13 @@ if __name__ == "__main__":
     lines = make_data()
 
     # Build the model.
-    text_model = markovify.NewlineText("\n".join(lines), state_size=3)
+    text_model = markovify.NewlineText("\n".join(lines), state_size=2)
 
     # Print three randomly-generated sentences of no more than 280 characters
     count = 0
     tool = language_tool_python.LanguageTool('en-US')
     while count < 10:
-        sentence = text_model.make_short_sentence(280)
-        if len(sentence) > 80 and len(tool.check(sentence)) < 3:
+        sentence = text_model.make_short_sentence(280, 80)
+        if len(tool.check(sentence)) < 5:
             print(tool.correct(sentence))
             count += 1
